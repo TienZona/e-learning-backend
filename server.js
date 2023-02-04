3e15287209d92512757eac2e2d9f7ca20c776018
TienZona <tienzona001@gmail.com>
2023-02-04 11:57:58 +0700

    update 4/2/2023

--- server.js
+++ server.js
@@ -13,20 +13,95 @@ const io = new Server(server, {
   },
 });
 
+var rooms = [];
+var host = {
+  username: "",
+  id: "",
+};
+
+const joinRoom = (id, user, socketID) => {
+  user.socketID = socketID;
+  if (rooms.find((item) => item.id === id)) {
+    rooms = rooms.map((item) => {
+      if (item.id === id) {
+        item.users.push(user);
+        item = {
+          id: item.id,
+          users: item.users,
+          data: item.data,
+        };
+      }
+
+      return item;
+    });
+  } else {
+    console.log("add new room");
+    const newRoom = {
+      id,
+      users: [user],
+      data: [],
+    };
+
+    host = {
+      username: user,
+      id,
+    };
+
+    rooms.push(newRoom);
+  }
+};
+
+const removeUSer = (roomID, socketID) => {
+  rooms = rooms.map((room) => {
+    if (room.id == roomID) {
+      const newUsers = room.users;
+
+      room.users.forEach((user, index) => {
+        if(user.socketID === socketID)
+          newUsers.splice(index, 1);
+      })
+
+      room.users = newUsers;
+    } 
+    return room;
+  });
+};
+
+const addData = (data) => {
+  rooms.map((room) => {
+    if (room.id === data.room) {
+      room.data = [data];
+      return room;
+    } else {
+      return room;
+    }
+  });
+};
+
+const selectRoom = (roomID) => {
+  return rooms.filter((item) => item.id == roomID)[0];
+};
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+  // when user join in room
+  socket.on("join_room", (roomId, user) => {
+    socket.join(roomId);
+    console.log(`User with ID: ${socket.id} joined room: ${roomId}`);
+    joinRoom(roomId, user, socket.id);
+    socket.to(roomId).emit("receive_user_join", selectRoom(roomId), user);
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
+    socket.on("send_message", (data) => {
+      addData(data);
+      console.log(data.author + " send message room " + data.room);
+      socket.to(data.room).emit("receive_message", selectRoom(roomId), data);
+    });
 
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
+    socket.on("disconnect", () => {
+      removeUSer(roomId, socket.id);
+      console.log("User Disconnected", socket.id);
+    });
   });
 });
 
